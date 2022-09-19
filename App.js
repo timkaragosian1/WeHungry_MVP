8b6925393dda4067b5bbae4e6eb5d06090aa904f
timkaragosian1 <timkaragosian@gmail.com>
2022-09-19 08:18:47 -0400

    test commit

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import NewPasswordScreen from './src/screens/NewPasswordScreen';
 import Navigation from './src/nagivation';
 import HomeScreen from './src/screens/HomeScreen';
 import RestaurantsScreen from './src/screens/RestaurantsScreen/RestaurantsScreen';
-//import * as SQLite from 'expo-sqlite';
+//import * as SQLite from 'expo-sqlite' ;
 
 //global.db = SQLite.openDatabase('./assets/db/WeHungryDb.db');
 
